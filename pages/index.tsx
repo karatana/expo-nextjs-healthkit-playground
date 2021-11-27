9c7b0b53c420ceea914163b160089cc82636ae50
karatana <7781183+karatana@users.noreply.github.com>
2021-11-27 19:12:05 +0900

    :zap: 別ページを追加（ネイティブだと動かない）

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 // @generated: @expo/next-adapter@2.1.52
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Link from 'next/link';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+      <>
+        <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+        <Link href="/about">
+          <>
+            <Text style={styles.spacer}></Text>
+            <Text style={styles.link}>About</Text>
+          </>
+        </Link>
+      </>
     </View>
   );
 }
@@ -18,5 +27,12 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 16,
+  },
+  spacer: {
+    height: '20px',
+  },
+  link: {
+    color: 'blue',
+    cursor: 'pointer',
   },
 });
